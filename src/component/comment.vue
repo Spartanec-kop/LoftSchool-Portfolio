a6d30cf4d6deafa9862fcd799ad062b4d6262aa5
Spartanec-kop <spartanec_kop@mail.ru>
2020-02-09 20:30:06 +0300

    reviews on portfolio

--- src/component/comment.vue
+++ src/component/comment.vue
@@ -7,12 +7,12 @@
       .comment-item-text
         p {{comment.text}}
       .comment-item-person
-        .comment-item-person-avatar аватар
-          img.avatar-img(:src="createImageUrl(comment.avatar)")
+        .comment-item-person-avatar
+          img.avatar-img(:src="'https://webdev-api.loftschool.com/' + comment.photo")
         .comment-item-person-Name
-          span.person-Name {{comment.name}}
+          span.person-Name {{comment.author}}
           br
-          span.person-position {{comment.position}}  
+          span.person-position {{comment.occ}}  
 </template>
 <script>
 export default {
