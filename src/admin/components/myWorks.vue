8e06bb91108b3e077fe17f8edf2d67cfecb6a90c
Spartanec-kop <spartanec_kop@mail.ru>
2020-02-12 06:47:17 +0300

    formated

--- src/admin/components/myWorks.vue
+++ src/admin/components/myWorks.vue
@@ -119,80 +119,80 @@
         )    
 </template>
 <script>
-import SimpleVueValidator from 'simple-vue-validator';
+import SimpleVueValidator from "simple-vue-validator";
 const Validator = SimpleVueValidator.Validator;
 
-import tag from './tag'
-import work from './work'
+import tag from "./tag";
+import work from "./work";
 export default {
   mixins: [SimpleVueValidator.mixin],
-  components:{tag, work},
-  name: 'myWorks',
-  data(){
-    return{
+  components: { tag, work },
+  name: "myWorks",
+  data() {
+    return {
       currentWork: null,
-      works:[],
+      works: []
+    };
+  },
+  validators: {
+    "currentWork.title"(value) {
+      return Validator.value(value).required("Поле не должно быть пустым");
+    },
+    "currentWork.description"(value) {
+      return Validator.value(value).required("Поле не должно быть пустым");
+    },
+    "currentWork.link"(value) {
+      return Validator.value(value).required("Поле не должно быть пустым");
+    },
+    "currentWork.techs"(value) {
+      return Validator.value(value).required("Поле не должно быть пустым");
+    },
+    "currentWork.photo"(value) {
+      return Validator.value(value).required("Нужно загрузить фото");
     }
   },
-  validators:{
-    'currentWork.title'(value){
-      return Validator.value(value).required('Поле не должно быть пустым');
-    },
-    'currentWork.description'(value){
-      return Validator.value(value).required('Поле не должно быть пустым');
-    },
-    'currentWork.link'(value){
-      return Validator.value(value).required('Поле не должно быть пустым');
-    },
-    'currentWork.techs'(value){
-      return Validator.value(value).required('Поле не должно быть пустым');
-    },
-    'currentWork.photo'(value){
-      return Validator.value(value).required('Нужно загрузить фото');
-    }
-  },
-  methods:{
-    removeTag(val){
-      let tags = this.currentWork.techs.split(', ')
+  methods: {
+    removeTag(val) {
+      let tags = this.currentWork.techs.split(", ");
       tags.forEach((element, i) => {
-        if(element == val){
+        if (element == val) {
           tags.splice(i, 1);
         }
-      this.currentWork.techs = tags.join(', ');
+        this.currentWork.techs = tags.join(", ");
       });
     },
-    uploadImage(){
+    uploadImage() {
       this.$refs.workImage.click();
     },
-    changeImgFile(e){
+    changeImgFile(e) {
       this.currentWork.photo = e.target.files[0];
       var reader = new FileReader();
 
       reader.onload = function(event) {
-        var imgtag = document.getElementById("edit-img-preview");;
+        var imgtag = document.getElementById("edit-img-preview");
         imgtag.src = event.target.result;
       };
 
       reader.readAsDataURL(this.currentWork.photo);
     },
-    nameChange(value){
+    nameChange(value) {
       this.currentWork.title = value;
-      
     },
-    linkChange(value){
+    linkChange(value) {
       this.currentWork.link = value;
     },
-    descChange(value){
+    descChange(value) {
       this.currentWork.description = value;
     },
-    tagsChange(value){
+    tagsChange(value) {
       this.currentWork.techs = value;
     },
-    selectWork(work){
-      this.currentWork = {...work};
+    selectWork(work) {
+      this.currentWork = { ...work };
     },
-    removeWork(work){
-      this.$axios.delete(`/works/${work.id}`)
+    removeWork(work) {
+      this.$axios
+        .delete(`/works/${work.id}`)
         .then(Response => {
           this.works.splice(this.works.indexOf(work), 1);
         })
@@ -200,45 +200,44 @@ export default {
           console.log(error.Response);
         });
     },
-    cancelEdit(){
+    cancelEdit() {
       this.currentWork = null;
     },
-    saveEdit(){
-      this.$validate()
-        .then(success => {
-          if (success) {
-            if(!this.currentWork.id){
-              var formData = new FormData();
-              formData.append("title", this.currentWork.title);
-              formData.append("techs", this.currentWork.techs);
-              formData.append("photo", this.currentWork.photo);
-              formData.append("link", this.currentWork.link);
-              formData.append("description", this.currentWork.description);
-              this.$axios.post(`/works`, formData, {
-                                              headers: {
-                                                'Content-Type': 'multipart/form-data'
-                                              }
-                })
+    saveEdit() {
+      this.$validate().then(success => {
+        if (success) {
+          if (!this.currentWork.id) {
+            var formData = new FormData();
+            formData.append("title", this.currentWork.title);
+            formData.append("techs", this.currentWork.techs);
+            formData.append("photo", this.currentWork.photo);
+            formData.append("link", this.currentWork.link);
+            formData.append("description", this.currentWork.description);
+            this.$axios
+              .post(`/works`, formData, {
+                headers: {
+                  "Content-Type": "multipart/form-data"
+                }
+              })
               .then(Response => {
                 this.works.push(Response.data);
               })
               .catch(error => {
                 console.log(error.Response);
               });
-              
-            }
-            else{
-              var formData = new FormData();
-              formData.append("title", this.currentWork.title);
-              formData.append("techs", this.currentWork.techs);
-              formData.append("photo", this.currentWork.photo);
-              formData.append("link", this.currentWork.link);
-              formData.append("description", this.currentWork.description);
-              this.$axios.post(`/works/${this.currentWork.id}`, formData, {
-                                              headers: {
-                                                'Content-Type': 'multipart/form-data'
-                                              }
-                })
+          } else {
+            var formData = new FormData();
+            formData.append("title", this.currentWork.title);
+            formData.append("techs", this.currentWork.techs);
+            formData.append("photo", this.currentWork.photo);
+            formData.append("link", this.currentWork.link);
+            formData.append("description", this.currentWork.description);
+            this.$axios
+              .post(`/works/${this.currentWork.id}`, formData, {
+                headers: {
+                  "Content-Type": "multipart/form-data"
+                }
+              })
               .then(Response => {
                 let tmp = this.works.find(f => f.id == this.currentWork.id);
                 this.works[this.works.indexOf(tmp)] = Response.data.work;
@@ -247,76 +246,76 @@ export default {
               .catch(error => {
                 console.log(error.Response);
               });
-            }
           }
-        })      
+        }
+      });
     },
-    addNewWork(){
+    addNewWork() {
       this.currentWork = {
-          id:null,         
-          title: '',
-          photo: null,
-          link:'',
-          description:'',
-          techs:''
-
-        };
-      this.validation.reset();  
+        id: null,
+        title: "",
+        photo: null,
+        link: "",
+        description: "",
+        techs: ""
+      };
+      this.validation.reset();
     }
   },
-  beforeMount(){
-    this.$axios.get(`/works/${this.$user.id}`)
-    .then(Response => {
-      this.works = Response.data;
-    })
-    .catch(error => {
-      console.log(error.Response);
-    });
+  beforeMount() {
+    this.$axios
+      .get(`/works/${this.$user.id}`)
+      .then(Response => {
+        this.works = Response.data;
+      })
+      .catch(error => {
+        console.log(error.Response);
+      });
   }
-}
+};
 </script>
 <style lang="postcss" scoped>
-#work-photo{
+#work-photo {
   display: none;
 }
-.works-wrapper{
+.works-wrapper {
   padding-bottom: 40px;
 }
-.title{
+.title {
   display: flex;
   align-items: center;
   padding: 20px 0;
 }
-.title-text{
+.title-text {
   font-size: 21px;
   font-weight: bold;
 }
-.edit-works{
+.edit-works {
   padding: 30px;
   background-color: white;
   box-shadow: 4.1px 2.9px 20px 0 rgba(0, 0, 0, 0.07);
 }
-.edit-works-title{
+.edit-works-title {
   padding-bottom: 25px;
   padding-left: 10px;
   font-size: 18px;
   font-weight: bold;
   line-height: 1.89;
 }
-hr{
+hr {
   opacity: 0.15;
 }
-.edit-works-body{
+.edit-works-body {
   display: grid;
   grid-template-columns: 1fr 1fr;
   column-gap: 30px;
   padding-top: 45px;
 }
 .import-image,
-.works-description{
-  padding:10px;
+.works-description {
+  padding: 10px;
 }
-.admin-preview-text{
+.admin-preview-text {
   font-size: 16px;
   font-weight: 600;
   line-height: 2.13;
@@ -329,18 +328,18 @@ hr{
   width: 100%;
   padding: 10px;
 }
-.import-image{
+.import-image {
   background-color: #dee4ed;
   background-image: url("data:image/svg+xml;utf8,<svg width='100%' height='100%' xmlns='http://www.w3.org/2000/svg'><rect width='100%' height='100%' style='fill: none; stroke: darkgrey; stroke-width: 2; stroke-dasharray: 10 10'/></svg>");
   position: relative;
-  width: 100%; 
+  width: 100%;
 }
-.import-image{
+.import-image {
   display: block;
-  padding-top: 56%;    
+  padding-top: 56%;
 }
-.import-image-content{
-  position:  absolute;
+.import-image-content {
+  position: absolute;
   top: 0;
   left: 0;
   bottom: 0;
@@ -350,65 +349,65 @@ hr{
   justify-content: center;
   text-align: center;
 }
-.import-image-text{
+.import-image-text {
   opacity: 0.5;
   font-size: 16px;
   font-weight: 600;
   line-height: 2.13;
 }
-.fill-button-wrapper{
+.fill-button-wrapper {
   width: 181px;
   height: 50px;
   margin: 0 auto;
   margin-top: 27px;
 }
-.admin-input{
-  &:nth-child(4){
+.admin-input {
+  &:nth-child(4) {
     margin-bottom: 0px;
   }
 }
-.admin-tags{
+.admin-tags {
   display: flex;
   padding-top: 20px;
 }
-.edit-tag{
+.edit-tag {
   margin-right: 10px;
-  &:last-child{
+  &:last-child {
     margin-right: 0px;
   }
 }
-.desc-work{
-  height:190px;
+.desc-work {
+  height: 190px;
 }
-.edit-works-buttons{
+.edit-works-buttons {
   display: flex;
   justify-content: flex-end;
   align-items: center;
   padding-top: 40px;
 }
-.edit-works-cancel{
+.edit-works-cancel {
   font-weight: 600;
   line-height: 2.13;
   color: #383bcf;
   padding-right: 60px;
   cursor: pointer;
 }
-.edit-works-save{
+.edit-works-save {
   height: 60px;
-  width: 180px
+  width: 180px;
 }
-.works-list{
+.works-list {
   display: grid;
   padding-top: 30px;
   grid-template-columns: 1fr 1fr 1fr;
   column-gap: 30px;
   row-gap: 30px;
 }
-.works-list-item{
+.works-list-item {
   box-shadow: 4.1px 2.9px 20px 0 rgba(0, 0, 0, 0.07);
   background-color: white;
 }
-.add-new-work{
+.add-new-work {
   background-image: linear-gradient(to right, #006aed, #3f35cb);
   display: flex;
   justify-content: center;
@@ -417,81 +416,80 @@ hr{
   cursor: pointer;
   padding: 40px 0;
 }
-.plus{
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    width: 95px;
-    height: 95px;
-    font-weight: 600;
-    line-height: 1;
-    color: #ffffff;
-    background: transparent;
-    border-radius: 50%;
-    border: 2px solid white;  
-    cursor: pointer;  
-    font-size: 72px;
-    font-weight: 300;
-  }
+.plus {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 95px;
+  height: 95px;
+  font-weight: 600;
+  line-height: 1;
+  color: #ffffff;
+  background: transparent;
+  border-radius: 50%;
+  border: 2px solid white;
+  cursor: pointer;
+  font-size: 72px;
+  font-weight: 300;
+}
 
-.add-new-work-text{
+.add-new-work-text {
   font-size: 18px;
   font-weight: bold;
   line-height: 1.67;
   text-align: center;
   color: #ffffff;
   padding-top: 30px;
-
 }
-.add-new-work-text span{
-      display: block;
-    }
+.add-new-work-text span {
+  display: block;
+}
 
 @media screen and (max-width: 768px) {
-  .edit-works-body{
+  .edit-works-body {
     grid-template-columns: 1fr;
     column-gap: 30px;
     max-width: 470px;
     margin: 0 auto;
   }
-  .edit-works-buttons{
+  .edit-works-buttons {
     justify-content: center;
   }
-  .works-list{
+  .works-list {
     grid-template-columns: 1fr 1fr;
   }
-}  
+}
 
 @media screen and (max-width: 500px) {
-  .works-list{
+  .works-list {
     display: grid;
     grid-template-columns: 1fr;
   }
-  .add-new-work{
+  .add-new-work {
     height: 110px;
     display: flex;
     justify-content: center;
     align-items: center;
     flex-direction: row;
   }
-  .add-new-work-text{
+  .add-new-work-text {
     padding-top: 0px;
     padding-left: 20px;
   }
-  .add-new-work-text span{
+  .add-new-work-text span {
     display: inline;
-    &:last-child{
-      &::before{
-        content: ' ';
+    &:last-child {
+      &::before {
+        content: " ";
       }
     }
   }
-  .plus{
+  .plus {
     width: 50px;
     height: 50px;
     font-size: 24px;
   }
-  .container{
+  .container {
     width: 100%;
   }
 }
--- src/admin/main.js
+++ src/admin/main.js
@@ -1,76 +1,72 @@
-import Vue from 'vue';
-import App from './App.vue'
-import VueRouter from 'vue-router'
-import axios from './requests'
+import Vue from "vue";
+import App from "./App.vue";
+import VueRouter from "vue-router";
+import axios from "./requests";
 import "core-js/stable";
 import "regenerator-runtime/runtime";
-import {store} from './store'
+import { store } from "./store";
 
-import login from './components/login'
-import maincontent from "./components/maincontent"
-import picturePlugin from './utils/ImgSvg.plugin'
-import plus from './components/plus.vue'
-import fillButton from './components/fill-button.vue'
-import adminInput from './components/adminInput'
-
+import login from "./components/login";
+import maincontent from "./components/maincontent";
+import picturePlugin from "./utils/ImgSvg.plugin";
+import plus from "./components/plus.vue";
+import fillButton from "./components/fill-button.vue";
+import adminInput from "./components/adminInput";
 
 Vue.prototype.$axios = axios;
 Vue.prototype.$baseUrl = "https://webdev-api.loftschool.com/";
 
 const router = new VueRouter({
-  routes:[
+  routes: [
     {
-      path: '/',
-      name: 'home',
+      path: "/",
+      name: "home",
       component: maincontent
     },
-    { 
-      path: '/login',
-      name:'login', 
+    {
+      path: "/login",
+      name: "login",
       component: login
     }
   ]
-})
-async function isAuthentificated(){
+});
+async function isAuthentificated() {
   let result = false;
-  if (localStorage.getItem('token')){
-    await axios.get('/user')
-    .then(response =>{
-      Vue.prototype.$user = response.data.user
-      result = true;
-    })
-    .catch( error => {
-      result = false;
-    });
-  }
-  else{
+  if (localStorage.getItem("token")) {
+    await axios
+      .get("/user")
+      .then(response => {
+        Vue.prototype.$user = response.data.user;
+        result = true;
+      })
+      .catch(error => {
+        result = false;
+      });
+  } else {
     result = false;
   }
-  return result
+  return result;
 }
 
 router.beforeEach((to, from, next) => {
-  if (to.path != '/login') {
-    !isAuthentificated()
-    .then(result => {
-      if (!result){
-        next('/login')
+  if (to.path != "/login") {
+    !isAuthentificated().then(result => {
+      if (!result) {
+        next("/login");
+      } else {
+        next();
       }
-      else{
-        next()
-      }
-    })    
+    });
+  } else {
+    next();
   }
-  else {
-    next()
-  }
-})
+});
 
-Vue.component('plus', plus);
-Vue.component('fill-button', fillButton);
-Vue.component('admin-input', adminInput);
-Vue.use(VueRouter)
-Vue.use(picturePlugin)
+Vue.component("plus", plus);
+Vue.component("fill-button", fillButton);
+Vue.component("admin-input", adminInput);
+Vue.use(VueRouter);
+Vue.use(picturePlugin);
 
 Vue.prototype.$axios = axios;
 
@@ -78,5 +74,5 @@ new Vue({
   el: "#app-root",
   render: h => h(App),
   router,
-  store,
+  store
 });
